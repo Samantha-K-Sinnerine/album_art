61f2f9d31ff97009922e3f5d9e61ea3619dbe2ca
Samantha-K-Sinnerine <sksinnerine@gmail.com>
2024-05-05 17:25:36 -0400

    added logic for search and 404 endpoints

--- index.js
+++ index.js
@@ -1,47 +1,56 @@
 const http = require('http');
 const port = 3000;
-const server = http.createServer() ;
-const fs= require('fs');
+const server = http.createServer();
+const fs = require('fs');
 
-server.on("request", connection_handler) ;
+server.on("request", connection_handler);
 function connection_handler(req, res) {
     console.log(`New Request for ${req.url} from ${req.socket.remoteAddress}`);
-    if(req.url === "/") {
+    if (req.url === "/") {
         const main = fs.createReadStream("html/main.html");
-        res.writeHead(200, {"Content-Type": "text/html"}); 
+        res.writeHead(200, { "Content-Type": "text/html" });
         //pipes the html file to our client in reesponse to the request
         // Pipe by default will close the response once the stream is finished so there is no need for res.end()
-        main.pipe(res) ; //readable stream to writable stream
-    } 
-    else if(req.url === "/favicon.ico") {
+        main.pipe(res); //readable stream to writable stream
+    }
+    else if (req.url === "/favicon.ico") {
         const favicon = fs.createReadStream("images/favicon.ico");
-        res.writeHead(200, {"Content-Type": "text/html"}); 
-        favicon.pipe(res) ;
+        res.writeHead(200, { "Content-Type": "image/x-icon" });
+        favicon.pipe(res);
     }
-    else if(req.url === "/images/banner.png") {
+    else if (req.url === "/images/banner.png") {
         const banner = fs.createReadStream("images/banner.png");
-        res.writeHead(200, {"Content-Type": "image/png"}); 
-        banner.pipe(res);       
+        res.writeHead(200, { "Content-Type": "image/png" });
+        banner.pipe(res);
     }
-    else if(req.url.startsWith("/album-art/")) {
-        res.writeHead(200, {"Content-Type": "text/html"}); ; 
-        res.end("replace with album-art")           
+    else if (req.url.startsWith("/album-art/")) {
+        const image_stream = fs.createReadStream(`.${req.url}`);
+        image_stream.on('error', image_error_handler);
+        function image_error_handler(err) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.write("404 Not Found", () => res.end());
+        }
+        image_stream.on('ready', deliver_image);
+        function deliver_image() {
+            res.writeHead(200, { "Content-Type": "image/jpeg" });
+            image_stream.pipe(res);
+        }
+    }   
+    else if (req.url.startsWith("/search")) {
+            const url = new URL(req.url + "http://localhost:3000");
+            const artist = url.searchParams.get("artist");
     }
-    else if(req.url.startsWith("/search")) {
-        res.writeHead(200, {"Content-Type": "text/html"}); 
-        res.end("replace with search")       
+        else {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.write("404 Not Found", () => res.end());
+        }
     }
-    else {
-        res.writeHead(200, {"Content-Type": "text/html"}); 
-        res.end("replace with 404")
-    }
-}
 
-server.on("listening", listening_handler) ;
-server.listen(port) ;
-function listening_handler() {
-    console.log (`Now Listening on Port ${port}`);
-}
+    server.on("listening", listening_handler);
+    server.listen(port);
+    function listening_handler() {
+        console.log(`Now Listening on Port ${port}`);
+    }
 
 /*
 first request is for the root of our site --> /
